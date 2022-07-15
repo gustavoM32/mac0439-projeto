b08b827af0948b2c063a3b34f40acf3bac2e4124
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-07-15 05:01:12 -0300

    Add CommandLineRunner to application main class

--- src/main/kotlin/com/mac0439/projeto/ProjetoApplication.kt
+++ src/main/kotlin/com/mac0439/projeto/ProjetoApplication.kt
@@ -1,10 +1,19 @@
 package com.mac0439.projeto
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ProjetoApplication
+class ProjetoApplication() : CommandLineRunner {
+	private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+	override fun run(vararg args: String?) {
+		logger.info("Iniciou")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<ProjetoApplication>(*args)
