5a79441fb843eaf54f31eefb06ddcc6df29c07f1
Lara <laraayumi56@gmail.com>
2022-07-19 00:38:37 -0300

    Change stuff

--- src/main/kotlin/com/mac0439/projeto/controllers/TaskController.kt
+++ src/main/kotlin/com/mac0439/projeto/controllers/TaskController.kt
@@ -1,7 +1,9 @@
 package com.mac0439.projeto.controllers
 
+import com.mac0439.projeto.domain.neo4j.project.Project
 import com.mac0439.projeto.domain.neo4j.repositories.UserRepository
 import com.mac0439.projeto.domain.neo4j.task.Task
+import com.mac0439.projeto.services.ProjectService
 import com.mac0439.projeto.services.TaskService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*
 
 @Controller
 class TaskController(private val service: TaskService,
+                     private val projectService: ProjectService,
                      private val user_repository: UserRepository) {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
     @GetMapping("/tasks")
@@ -19,6 +22,28 @@ class TaskController(private val service: TaskService,
         model.addAttribute("tasks", service.findAll())
         return "tasks/index"
     }
+
+//    @GetMapping("/projects/{cid}/tasks")
+//    fun getTaskas(@PathVariable cid: String, model: Model): String {
+//        logger.info("get /projects/$cid/tasks")
+//        val project : Project
+//
+//        try {
+//            project = projectService.findById(cid).get()
+//            //community = communityService.findById(cid)
+//        } catch (e: Exception) {
+//            logger.error(e.toString())
+//            return "redirect:/projects"
+//        }
+//        model.addAttribute("tasks", project.task_list?.sortedByDescending { it.date } ?: listOf<Task>())
+//        model.addAttribute("project", project)
+//        //model.addAttribute("taskStatus", taskStatus)
+////        model.addAttribute("events", community.events?.sortedByDescending { it.date } ?: listOf<Event>())
+////        model.addAttribute("community", community)
+////        model.addAttribute("eventStatus", eventStatus)
+//        return "projects/tasks/index"
+//    }
+
 
     @GetMapping("tasks/{id}")
     fun getTasks(@PathVariable id: String, model: Model): String {
